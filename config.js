54f70b821bd3e96b02611b1a050d32e5780c0857
King Dave <mickyyoung660@gmail.com>
2026-06-19 13:54:06 +0100

    Set ALWAYS_ONLINE to true by default

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
     OWNER_NUMBER: process.env.OWNER_NUMBER || "2347086613483",
     WORKTYPE: process.env.WORKTYPE || "private",
     PREFIX: process.env.PREFIX || "",
-    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || false,
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || true,
     MONGODB_URI: process.env.MONGODB_URI || "",
     STATUS_VIEW: process.env.STATUS_VIEW || false,
     SAVE_STATUS: process.env.SAVE_STATUS || false,
